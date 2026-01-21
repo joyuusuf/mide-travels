4ff0e3f31c015ad5ff11d48326e863bb259bf5d5
Yuusuf Jawad Olamide <yuusufjawadolamide67@gmail.com>
2026-01-21 16:59:40 +0100

    new commit

--- src/app/Index/page.jsx
+++ src/app/Index/page.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+"use client"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocation } from "@fortawesome/free-brands-svg-icons";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function page() {
+
+export default function Index() {
   return (
-    <div>page</div>
+    <>
+    <div className="hero h-screen min-h-screen flex justify-center items-center z-10">
+        <div className="hero-content relative text-center ">
+            <h1 className="xl:text-8xl lg:text-7xl md:text-6xl text-4xl unbounded-font font-bold text-white">
+                Find Your Best <br/> travels Package
+            </h1>
+
+            <p className="pt-3 text-[#ffffffb3] md:pb-10 pb-5">
+                Planning a trip? We will organize toyur trip with the best places to visit.
+            </p>
+
+            <button className="btn bg-white group text-[#193555] hover:bg-[#193555] font-bold px-6 w-auto py-4 rounded-full 
+            cursor-pointer transition-colors duration-300">
+                <a href="#" className="unbounded-font text-sm xl:text-md uppercase group-hover:text-white transition-colors duration-300 tracking-wider">
+                    View All Tours
+                </a>
+            </button>
+        </div>
+    </div>
+    
+    </>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+
+import Index from "./Index/page";
 
 export default function Home() {
   return (
-    <div>
-    
-    </div>
+    <>
+    <Index />
+    </>
   );
 }
